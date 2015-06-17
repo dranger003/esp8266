7264fa605ce43d322054d96d9229f00d8bc423b2
DAN™ <dranger003@gmail.com>
2015-06-16 20:02:29 -0400

    Flash script path update.

--- rboot-sampleproject/write_flash.sh
+++ rboot-sampleproject/write_flash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash -fs 4m 0x00000 ../rboot/rboot.bin 0x02000 firmware/rom0.bin && screen /dev/ttyUSB0 115200
+/usr/bin/esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash -fs 4m 0x00000 ../rboot/firmware/rboot.bin 0x02000 firmware/rom0.bin && screen /dev/ttyUSB0 115200
